227750ea4663f767bf803bce13271c4d0a44c3dd
Przemek Hussar <hayss007@gmail.com>
2025-05-24 18:54:02 +0200

    fix font for deplay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,36 @@
 import type { Metadata } from "next";
 import { Lato } from "next/font/google";
 import "./globals.css";
-import {Header} from "@/components/Header/Header";
-import {ClientProviders} from "@/context/ClientProviders";
-
-;
+import { Header } from "@/components/Header/Header";
+import { ClientProviders } from "@/context/ClientProviders";
 
 const LatoSans = Lato({
     weight: ['300', '400', '700'],
+    subsets: ['latin'],
+    display: 'swap',
 });
 
 export const metadata: Metadata = {
-  title: "Nova Moda",
-  description: "Your favorite clothing store",
+    title: "Nova Moda",
+    description: "Your favorite clothing store",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pl">
-      <body className={LatoSans.className}>
-      <ClientProviders>
-          <div>
-              <Header/>
-              {children}
-              {/*<Footer/>*/}
-          </div>
-      </ClientProviders>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="pl">
+        <body className={LatoSans.className}>
+        <ClientProviders>
+            <div>
+                <Header />
+                {children}
+                {/* <Footer /> */}
+            </div>
+        </ClientProviders>
+        </body>
+        </html>
+    );
 }
