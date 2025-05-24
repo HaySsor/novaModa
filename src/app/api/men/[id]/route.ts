33b5715a8c9cede36adcb2528da26031f2480bd3
Przemek Hussar <hayss007@gmail.com>
2025-05-24 19:28:44 +0200

    fix some issue on deplay

--- src/app/api/men/[id]/route.ts
+++ src/app/api/men/[id]/route.ts
@@ -1,17 +1,21 @@
+// src/app/api/men/[id]/route.ts
 import { NextResponse } from 'next/server'
 import rawData from '@/data/MenClothes.json'
 import type { ClothesPage } from '@/Types/ClothesPage'
 
 const dataJson = rawData as unknown as ClothesPage
 
-interface Params {
-    params: { id: string }
-}
+export async function GET(
+    request: Request,
+    { params }: { params: { id: string } }
+) {
+    const { id } = params
+    const category = dataJson[id]
 
-export async function GET(_: Request, { params }: Params) {
-    const category = dataJson[params.id]
     if (!category) {
+        // zwracamy pierwszy argument jako null, a status 404
         return new Response(null, { status: 404 })
     }
+
     return NextResponse.json(category)
 }
